b5aee103be311aa63bccac4d139022930242ca26
Prabhu <prabhuelectro99@gmail.com>
2020-06-21 16:19:06 +0530

    Update build.sh

--- opencv/build.sh
+++ opencv/build.sh
@@ -2,7 +2,7 @@ echo "Build Script for building OpenCV-4"
 
 echo "Installing requirement's"
 
-sudo apt install build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev -y
+sudo apt install build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libdc1394-22-dev -y
 
 echo "Sucessfully installed requirement's"
 
