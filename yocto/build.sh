064a9040a8971e37b7e0ee7a0c1a905c74dbb548
Prabhu <prabhuelectro99@gmail.com>
2020-07-02 18:48:21 +0530

    Update build.sh

--- yocto/build.sh
+++ yocto/build.sh
@@ -1,1 +1,28 @@
+echo "Yocto Building script"
+
+echo "Updating APT"
+
+sudo apt update -y
+
+echo "Installing requirements"
+
+sudo apt install build-essential chrpath diffstat gawk libncurses5-dev texinfo git wget curl 
+
+echo "Start fetching repos"
+
+mkdir -pv yocto/
+
+cd yocto/
+
+git clone -b dunfell git://git.yoctoproject.org/poky.git poky-dunfell
+
+git clone -b dunfell git://git.openembedded.org/meta-openembedded
+
+git clone -b dunfell https://github.com/meta-qt5/meta-qt5
+
+git clone -b dunfell git://git.yoctoproject.org/meta-raspberrypi
+
+git clone -b dunfell git://git.yoctoproject.org/meta-security.git
+
+git clone -b dunfell https://github.com/jumpnow/meta-jumpnow
 
