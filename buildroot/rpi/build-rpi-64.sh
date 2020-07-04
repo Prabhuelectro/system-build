595d807bf1e96f8bdbdfd0587b4208df42d6894a
Prabhu <prabhuelectro99@gmail.com>
2020-07-04 06:57:30 +0530

    Create build-rpi-64.sh

---
+++ buildroot/rpi/build-rpi-64.sh
@@ -0,0 +1,11 @@
+sudo apt update -y
+
+sudo apt install gcc git build-essential wget cpio unzip rsync bc gcc-aarch64-linux-gnu openssh-client -y
+
+mkdir -pv linux-build/
+
+cd linux-build/
+
+git clone -b 2020.05.x git://git.busybox.net/buildroot
+
+cp ../rpi
