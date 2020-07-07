cf8e345945e49271a8d869820d187883efad725c
Prabhu <prabhuelectro99@gmail.com>
2020-07-07 21:22:25 +0530

    Update build-rpi-64.sh

--- buildroot/rpi/build-rpi-64.sh
+++ buildroot/rpi/build-rpi-64.sh
@@ -1,6 +1,6 @@
 sudo apt update -y
 
-sudo apt install gcc git build-essential wget cpio unzip rsync bc -y
+sudo apt install gcc git build-essential wget cpio unzip rsync bc curl -y
 
 mkdir -pv linux-build/
 
