8893fef79881118352e0ecf3adcc94277ce21654
Prabhu <prabhuelectro99@gmail.com>
2020-10-22 17:21:42 +0530

    Update build.sh

--- opencv/build.sh
+++ opencv/build.sh
@@ -24,19 +24,19 @@ wget https://github.com/opencv/opencv_contrib/archive/4.4.0.tar.gz
 
 tar xvf 4.4.0.tar.gz
 
-cd opencv-4.4.0/platforms/linux
+cd opencv-4.4.0/
 
 mkdir -p build
 
 cd build
 
-cmake -D CMAKE_TOOLCHAIN_FILE=../aarch64-gnu.toolchain.cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=/usr -D ENABLE_VFPV3=ON -D ENABLE_NEON=ON-D OPENCV_GENERATE_PKGCONFIG=ON -D BUILD_DOCS=ON -D BUILD_EXAMPLES=ON -D WITH_OPENMP=ON -D WITH_CUDA=OFF -D OPENCV_ENABLE_NONFREE=ON -D BUILD_PERF_TESTS=OFF -D BUILD_TESTS=OFF -D OPENCV_EXTRA_MODULES_PATH=../../../../opencv_contrib-4.4.0/modules -D ENABLE_PRECOMPILED_HEADERS=OFF -D BUILD_opencv_java=OFF -D BUILD_opencv_python3=ON -DWITH_OPENCL=ON -DWITH_GTK=ON ../../..
+cmake -D CMAKE_TOOLCHAIN_FILE=../platforms/linux/aarch64-gnu.toolchain.cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=/usr/local -D ENABLE_VFPV3=ON -D ENABLE_NEON=ON-D OPENCV_GENERATE_PKGCONFIG=ON -D BUILD_DOCS=ON -D BUILD_EXAMPLES=ON -D WITH_OPENMP=ON -D WITH_CUDA=OFF -D OPENCV_ENABLE_NONFREE=ON -D BUILD_PERF_TESTS=OFF -D BUILD_TESTS=OFF -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-4.4.0/modules -D ENABLE_PRECOMPILED_HEADERS=OFF -D BUILD_opencv_java=OFF -D BUILD_opencv_python3=ON -DWITH_OPENCL=ON -DWITH_GTK=ON ../
 
 make
 
 echo "Building Opencv is sucessfull"
 
-cd ../../../
+cd ../../
 
 echo "Compressing Files"
 
